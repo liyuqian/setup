2d4ba04256ac8004f9b101d77adce5bf3da1149d
Yuqian Li <liyuqian79@gmail.com>
2023-11-26 14:44:38 -0800

    Change current dir from blue to cyan
    The blue color is sometimes hard to read with dark backgrounds.

--- lib/dotfile.dart
+++ lib/dotfile.dart
@@ -3,7 +3,7 @@ import 'package:setup/setup.dart';
 
 const Map<String, String> kRelativePathToHash = {
   '.zshrc': 'fd23131b',
-  '.p10k.zsh': '7a066a31',
+  '.p10k.zsh': '7cd917a1',
   '.tmux.conf.local': 'd9830edc',
   '.vim_runtime/my_configs.vim': '44d404ca',
   '.vim_runtime/my_plugins/coc_configs.vim': '1e5e28c0',
